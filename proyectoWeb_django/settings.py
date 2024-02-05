7571645fe770495bb68442b354a4caacfe56a35c
Fernanda Valenzuela <fe.valenzuelac@duocuc.cl>
2024-02-05 16:52:22 -0300

    modif staticfiles

--- proyectoWeb_django/settings.py
+++ proyectoWeb_django/settings.py
@@ -146,9 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "crud/static"),
+    os.path.join(BASE_DIR, "core/static"),
 ]
 
 #Media Files

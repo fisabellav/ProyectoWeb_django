1aeaaa4f5199499214cec9138cad8a123b912bd6
Fernanda Valenzuela <fe.valenzuelac@duocuc.cl>
2024-02-10 22:01:50 -0300

    media whitenoise

--- proyectoWeb_django/settings.py
+++ proyectoWeb_django/settings.py
@@ -148,6 +148,7 @@ WHITENOISE_MANIFEST_STRICT = False
 #Media Files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+WHITENOISE_MEDIA_LOCATION = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

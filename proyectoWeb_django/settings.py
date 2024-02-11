d98d61d400d16696217f9a7d0501de19aef83958
Fernanda Valenzuela <fe.valenzuelac@duocuc.cl>
2024-02-10 21:22:49 -0300

    modicicaciones settings media

--- proyectoWeb_django/settings.py
+++ proyectoWeb_django/settings.py
@@ -147,7 +147,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 #Media Files
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

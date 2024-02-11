94e0fbd8119a6f92689b3b01d80fcd7f458711b1
Fernanda Valenzuela <fe.valenzuelac@duocuc.cl>
2024-02-10 21:29:40 -0300

    cambios media

--- proyectoWeb_django/settings.py
+++ proyectoWeb_django/settings.py
@@ -144,9 +144,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
+WHITENOISE_MANIFEST_STRICT = False
 #Media Files
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 # Default primary key field type

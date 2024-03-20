99b326f7d9fc2c04abc28b6eb879d9c128f32f15
Fernanda Valenzuela <fe.valenzuelac@duocuc.cl>
2024-03-20 15:35:09 -0300

    se corrige base_dir

--- proyectoWeb_django/settings.py
+++ proyectoWeb_django/settings.py
@@ -16,7 +16,7 @@ import crispy_forms
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-secondary',
